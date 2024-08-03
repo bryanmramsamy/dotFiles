dd49d344937dfc4ea9ae598d9299e0e1b70552f5
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2024-08-03 14:14:34 +0200

    Upgrade `main.sh` script for polybar

--- .config/polybar/main.sh
+++ .config/polybar/main.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
-# Polybar launch script
+# Simplified Polybar launch script
 
-HOSTNAME="Cassiopeia-2022"
+# List of bars to launch
+BARS=(
+  top-monitor1
+  bottom-monitor1
+  top-monitor2
+  bottom-monitor2
+  top-monitor3
+  bottom-monitor3
+  top-monitor4
+  bottom-monitor4
+  top-monitor5
+  bottom-monitor5
+)
 
-killall -q polybar
+# Kill all existing Polybar instances
+pids=$(pgrep -x polybar)
+if [ -n "$pids" ]; then
+  kill -9 $pids
+fi
 
-xrdb $HOME/.Xresources # TODO: Should not be here
-
-case $HOSTNAME in
-"Butterfly-2019")
-  echo "Lauching polybars" | tee -a /tmp/polybar.log
-  polybar top-monitor1 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor1 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor2 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor2 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor3 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor3 >>/tmp/polybar.log 2>&1 &
-  echo "Polybars launched"
-  ;;
-"Cassiopeia-2022")
-  echo "Lauching polybars" | tee -a /tmp/polybar.log
-  polybar top-monitor1 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor1 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor2 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor2 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor3 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor3 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor4 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor4 >>/tmp/polybar.log 2>&1 &
-  polybar top-monitor5 >>/tmp/polybar.log 2>&1 &
-  polybar bottom-monitor5 >>/tmp/polybar.log 2>&1 &
-  echo "Polybars launched"
-  ;;
-esac
+# Launch Polybar on all configured monitors
+echo "Launching Polybars" | tee -a /tmp/polybar.log
+for bar in "${BARS[@]}"; do
+  polybar "$bar" >>/tmp/polybar.log 2>&1 &
+done
+echo "Polybars launched"
